c13986282629d56c9963b17c5727efdf1bdd5900
humtej1204 <humtej1204@gmail.com>
2021-11-07 00:12:35 +0000

    Adding code to _puts file

---
+++ _puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - function similar to puts but
+ * in my own version
+ * @c: char parameter
+ * Return: use the function write to print
+ * the string
+ */
+
+int _puts(char *c)
+{
+	int len;
+	len = _strlen(c) + 1;
+
+	return (write(1, c, len));
+}
