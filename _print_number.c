c93e4c197db88c86c1471720b1c38de004e22935
humtej1204 <humtej1204@gmail.com>
2021-11-07 00:08:57 +0000

    Adding code to _print_number file

---
+++ _print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _print_number - function to print numbers
+ *
+ * @n: Parameter
+ */
+void _print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		_print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
+}
