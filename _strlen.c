5421233856ef57cb02a440db7117c8e786478855
j4ir0st <jairocast2@gmail.com>
2021-11-06 17:21:48 -0700

    Adding _strlen function

---
+++ _strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function
+ * @s: pointer of parameter
+ * Return: return n's value
+ */
+int _strlen(char *s)
+{
+	int n = 0; /* Contador de caracteres */
+
+	while (*s != '\0')
+	{
+		n++;
+		s++; /* La direccion de s aumenta en 1 */
+	}
+
+	return(n); /* Retorna el valor final de n */
+}
