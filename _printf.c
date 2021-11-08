6d249e8206003fd1f5d2f61e13292051cb49d746
j4ir0st <jairocast2@gmail.com>
2021-11-08 09:46:08 -0800

    changing format of printf

--- _printf.c
+++ _printf.c
@@ -1,53 +1,26 @@
 #include "main.h"
+
 /**
  * _printf - my own printf function
- *
  * @format: Parameter
- *
- * Return: End program
+ * Return: c as counter
  */
+
 int _printf(const char *format, ...)
 {
-	int c = 0, num;
+	int c = 0;
+	int (*r_flag)(va_list list);
 
 	va_list ptr;
 	va_start(ptr, format);
+
 	while ((format != NULL) && *(format + c) != '\0')
 	{
-		if (*(format + c) == '%')
+		if (*(format + c) == '%' && *(format + c + 1) == '%')
 		{
-			switch (*(format + c + 1))
-			{
-			case 'c': /* char */
-				_putchar(va_arg(ptr, int));
-				c++;
-				break;
-
-			case 's': /* string */
-				_puts(va_arg(ptr, char *));
-				c++;
-				break;
-			case 'd': /* decimal */
-				num = va_arg (ptr, int);
-				_print_number(num);
-				c++;
-				break;
-			case 'i': /* integer */
-				num = va_arg (ptr, int);
-				_print_number(num);
-				c++;
-				break;
-			default:
-				_putchar('%');
-				c++;
-				continue;
-			}
+			_putchar('%');
+			c++;
 		}
-		else
-		{
-			_putchar(*(format + c));
-		}
-		c++;
 	}
 	va_end(ptr);
 	return (c);
