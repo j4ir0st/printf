946e7608c498938f97ec0e6d647c49aa6745311f
humtej1204 <humtej1204@gmail.com>
2021-11-08 18:15:40 +0000

    Correcting: Adding more code and correcting the betty Style

--- _printf.c
+++ _printf.c
@@ -10,8 +10,8 @@ int _printf(const char *format, ...)
 {
 	int c = 0;
 	int (*r_flag)(va_list list);
-
 	va_list ptr;
+
 	va_start(ptr, format);
 
 	while ((format != NULL) && *(format + c) != '\0')
@@ -21,7 +21,24 @@ int _printf(const char *format, ...)
 			_putchar('%');
 			c++;
 		}
+		else if (*(format + c) == '%')
+		{
+			r_flag = get_flag(format + c + 1);
+
+			if (r_flag != NULL)
+			{
+				r_flag(ptr);
+				c++;
+			}
+		}
+		else
+		{
+			_putchar(*(format + c));
+		}
+		c++;
 	}
+
 	va_end(ptr);
+
 	return (c);
 }
