caf5b53e84f83ee52cb3118fb7a52f60acd481dc
j4ir0st <jairocast2@gmail.com>
2021-11-09 13:09:59 -0800

    with betty style

--- _printf.c
+++ _printf.c
@@ -20,23 +20,6 @@ int _printf(const char *format, ...)
 	}
 	while (format != NULL && *(format + c) != '\0')
 	{
-		if (*(format + c) == '%')
-		{
-			if (format[c + 1] == ' ')
-                        {
-				if (format[c + 2] >= 48 && format[c + 2] <= 57)
-				{
-					return (-1);
-				}
-				else if (format[c + 2] == 32)
-				{
-					return (-1);
-				}
-			}
-			if (format[c + 1] >= 48 && format[c + 1] <= 57)
-			{
-				return (-1);
-			}
 			r_flag = get_flag(format + c + 1);
 			if (r_flag != NULL)
 			{
