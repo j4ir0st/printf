2dd35a366f6c40f52366e7478e497203174d488c
humtej1204 <humtej1204@gmail.com>
2021-11-09 08:50:18 +0000

    TEST

--- main.h
+++ main.h
@@ -1,12 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 /* ************* */
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
+
 /* ************* */
 int _putchar(char c);
 int _puts(char *c);
